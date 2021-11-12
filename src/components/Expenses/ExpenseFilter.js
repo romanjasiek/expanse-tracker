a3239ef1f645a7977500b3d080753ecbb3c57e14
Roman Jasiek <romanjasiek@me.com>
2021-11-12 21:07:26 +0100

    More small changes

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -7,8 +7,9 @@ const ExpensesFilter = (props) => {
     return (
       <div className='expenses-filter'>
         <div className='expenses-filter__control'>
-          <label>Filter by year</label>
+          <label>Filter by Year</label>
           <select value={props.selected} onChange={filterChangeHandler}>
+            <option value='2022'>2023</option>
             <option value='2022'>2022</option>
             <option value='2021'>2021</option>
             <option value='2020'>2020</option>
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -39,15 +39,15 @@ const ExpenseForm = (props) => {
         <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
-                    <label>Title</label>
+                    <label>What did you spend money on?</label>
                     <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
-                    <label>Amount</label>
+                    <label>How much was it?</label>
                     <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
-                    <label>Date</label>
+                    <label>When did you spend money on it?</label>
                     <input type="date" value={enteredDate} min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} />
                 </div>
 
